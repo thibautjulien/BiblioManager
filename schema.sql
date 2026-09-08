8def8d33e4cf74b618a2428bc6c65ec3d8e6c4a2
Thibaut Julien <thbtjulien21@gmail.com>
2026-09-08 10:55:45 +0200

    fix: ajout fichiers gitignore + pom

--- schema.sql
+++ schema.sql
@@ -1,1 +1,23 @@
 -- schema.sql
+
+CREATE TABLE livre (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titre TEXT NOT NULL,
+    auteur TEXT NOT NULL,
+    disponible BOOLEAN NOT NULL,
+);
+
+CREATE TABLE utilisateur (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL,
+    prenom TEXT NOT NULL,
+);
+
+CREATE TABLE emprunts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    livre_id INTEGER NOT NULL,
+    utilisateur_id INTEGER NOT NULL,
+
+    FOREIGN KEY (livre_id) REFERENCES livre(id),
+    FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id)
+);
